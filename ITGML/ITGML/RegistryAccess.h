c3df91152648a0f2c9b54537b581c4106e4a478d
sukibaby <163092272+sukibaby@users.noreply.github.com>
2025-05-02 06:31:33 -0700

    Update RegistryAccess.h

--- ITGML/ITGML/RegistryAccess.h
+++ ITGML/ITGML/RegistryAccess.h
@@ -1,8 +1,6 @@
 #pragma once
 
-// RegistryAccess.h
 #define REGISTRY_KEY L"SOFTWARE\\ITGML"
 #define REGISTRY_VALUE_NAME L"ExecutablePath"
 
-// Function declaration
 void LoadSettingsFromRegistry();
