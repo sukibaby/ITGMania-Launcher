3499c81084778d4fd33a8fead7f4091a84aba040
sukibaby <163092272+sukibaby@users.noreply.github.com>
2025-05-02 02:31:46 -0700

    reorganization, add header file

--- ITGML/ITGML/ITGML.cpp
+++ ITGML/ITGML/ITGML.cpp
@@ -1,4 +1,5 @@
-// ITGML.cpp : helper launcher for ITGmania.
+//
+// ITGML.cpp
 //
 
 #include "framework.h"
@@ -33,161 +34,8 @@ WCHAR szWindowClass[MAX_LOADSTRING];
 WCHAR selectedExePath[MAX_PATH] = L"";
 int selectedPriority = NORMAL_PRIORITY_CLASS;
 
-/*************************************/
-/******* Forward Declarations ********/
-/*************************************/
-ATOM MyRegisterClass(HINSTANCE hInstance);
-BOOL InitInstance(HINSTANCE, int);
-LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
-INT_PTR CALLBACK About(HWND, UINT, WPARAM, LPARAM);
-
 /*****************************************************************/
-/******************** Executable Modifiers ***********************/
-/*****************************************************************/
-#include <fstream>
-#include <shlobj.h> // For SHBrowseForFolder and SHGetPathFromIDList
-
-/* the purpose of this function is to remove the version check in any simply love
- * based theme.  it looks for the line containing the minimumversion variable
- * and overwrites it with a value of 0.0.1.  this allows the older exe to use
- * a newer version of the theme. */
-LPCWSTR targetLine = L"This will patch out the version check in any Simply Love theme.\nThe next step will ask you to locate your Simply Love folder.\nIf you use Zmod, then use your Zmod folder.";
-void SpoofExecutableVersion(const std::wstring& exePath) {
-    MessageBox(nullptr, targetLine, L"Info", MB_OK | MB_ICONINFORMATION);
-
-    BROWSEINFO bi = { 0 };
-    bi.lpszTitle = L"Select the Simply Love (or Zmod) folder";
-    LPITEMIDLIST pidl = SHBrowseForFolder(&bi);
-
-    if (!pidl) {
-        MessageBox(nullptr, L"Scripts folder selection canceled.", L"Info", MB_OK | MB_ICONINFORMATION);
-        return;
-    }
-
-	// this used to be the scripts folder, but it is now the simply love folder
-    WCHAR scriptsFolderPath[MAX_PATH];
-    if (!SHGetPathFromIDList(pidl, scriptsFolderPath)) {
-        MessageBox(nullptr, L"Failed to retrieve the Scripts folder path.", L"Error", MB_OK | MB_ICONERROR);
-        return;
-    }
-
-    CoTaskMemFree(pidl);
-
-    // Construct the path to SL-SupportHelpers.lua
-    std::wstring luaFilePath = std::wstring(scriptsFolderPath) + L"\\Scripts" + L"\\SL-SupportHelpers.lua";
-
-    // Open the Lua file for reading and writing
-    std::wifstream inputFile(luaFilePath);
-    if (!inputFile.is_open()) {
-        MessageBox(nullptr, L"Failed to open SL-SupportHelpers.lua for reading.", L"Error", MB_OK | MB_ICONERROR);
-        return;
-    }
-
-    std::wstring fileContent;
-    std::wstring line;
-    bool lineModified = false;
-
-    // Detect the line to modify
-    while (std::getline(inputFile, line)) {
-        if (line.find(L"local MinimumVersion") != std::wstring::npos) {
-            line = L"local MinimumVersion = {0, 0, 1}";
-            lineModified = true;
-        }
-        fileContent += line + L"\n";
-    }
-
-    inputFile.close();
-
-    // line is modified (or not), cleanup
-    if (!lineModified) {
-        MessageBox(nullptr, L"Target line not found in SL-SupportHelpers.lua.", L"Error", MB_OK | MB_ICONERROR);
-        return;
-    }
-
-    std::wofstream outputFile(luaFilePath, std::ios::trunc);
-    if (!outputFile.is_open()) {
-        MessageBox(nullptr, L"Failed to open SL-SupportHelpers.lua for writing.", L"Error", MB_OK | MB_ICONERROR);
-        return;
-    }
-
-    outputFile << fileContent;
-    outputFile.close();
-
-    MessageBox(nullptr, L"SL-SupportHelpers.lua modified successfully.", L"Success", MB_OK | MB_ICONINFORMATION);
-}
-
-void BrowseForExecutable(HWND hWnd) {
-    OPENFILENAME ofn;
-    WCHAR szFile[MAX_PATH];
-
-    ZeroMemory(&ofn, sizeof(ofn));
-    ofn.lStructSize = sizeof(ofn);
-    ofn.hwndOwner = hWnd;
-    ofn.lpstrFile = szFile;
-    ofn.lpstrFile[0] = '\0';
-    ofn.nMaxFile = sizeof(szFile) / sizeof(szFile[0]);
-    ofn.lpstrFilter = L"Executable Files\0*.exe\0All Files\0*.*\0";
-    ofn.nFilterIndex = 1;
-    ofn.Flags = OFN_PATHMUSTEXIST | OFN_FILEMUSTEXIST;
-
-    if (GetOpenFileName(&ofn) == TRUE) {
-        wcscpy_s(selectedExePath, ofn.lpstrFile);
-
-        // Store the path in the registry
-        HKEY hKey;
-        if (RegCreateKeyEx(HKEY_CURRENT_USER, REGISTRY_KEY, 0, nullptr, 0, KEY_WRITE, nullptr, &hKey, nullptr) == ERROR_SUCCESS) {
-            size_t dataSize = (wcslen(selectedExePath) + 1) * sizeof(WCHAR);
-            if (dataSize > MAXDWORD) {
-                MessageBox(nullptr, L"Path size exceeds the maximum allowed size. please report this to the ITGmania Launcher github.", L"Error", MB_OK | MB_ICONERROR);
-            }
-            else {
-                RegSetValueEx(hKey, REGISTRY_VALUE_NAME, 0, REG_SZ, reinterpret_cast<const BYTE*>(selectedExePath), static_cast<DWORD>(dataSize));
-            }
-            RegCloseKey(hKey);
-        }
-    }
-}
-
-void LaunchITGmania() {
-    HKEY hKey; // Check the registry for the executable path
-    if (RegOpenKeyEx(HKEY_CURRENT_USER, REGISTRY_KEY, 0, KEY_READ, &hKey) == ERROR_SUCCESS) {
-        DWORD bufferSize = sizeof(selectedExePath);
-        if (RegQueryValueEx(hKey, REGISTRY_VALUE_NAME, nullptr, nullptr, (LPBYTE)selectedExePath, &bufferSize) != ERROR_SUCCESS) {
-            selectedExePath[0] = L'\0'; // Clear the path if not found
-        }
-        RegCloseKey(hKey);
-    }
-
-    if (wcslen(selectedExePath) == 0) {
-        MessageBox(nullptr, L"Please select the executable first.",
-            L"Error", MB_OK | MB_ICONERROR);
-        return;
-    }
-
-
-    timeBeginPeriod(1);
-
-    LPCWSTR appArgs = L"";
-
-    SHELLEXECUTEINFO sei = { sizeof(SHELLEXECUTEINFO) };
-    sei.lpFile = selectedExePath;
-    sei.lpParameters = appArgs;
-    sei.nShow = SW_SHOWNORMAL;
-    sei.fMask = SEE_MASK_NOCLOSEPROCESS;
-
-    if (!ShellExecuteEx(&sei)) {
-        std::wcerr << L"Failed to launch application. Error: " << GetLastError() << std::endl;
-        return;
-    }
-
-    SetPriorityClass(sei.hProcess, selectedPriority);
-    WaitForSingleObject(sei.hProcess, INFINITE);
-    CloseHandle(sei.hProcess);
-    timeEndPeriod(1);
-}
-
-/*****************************************************************/
-/************************** GUI stuff ****************************/
+/************************** Entry Point **************************/
 /*****************************************************************/
 int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
     _In_opt_ HINSTANCE hPrevInstance,
@@ -214,9 +62,12 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
         }
     }
 
-    return (int)msg.wParam;
+    return static_cast<int>(msg.wParam);
 }
 
+/*****************************************************************/
+/************************** GUI Functions ************************/
+/*****************************************************************/
 ATOM MyRegisterClass(HINSTANCE hInstance) {
     WNDCLASSEXW wcex;
 
@@ -234,19 +85,6 @@ ATOM MyRegisterClass(HINSTANCE hInstance) {
     wcex.hIconSm = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
 
     return RegisterClassExW(&wcex);
-}
-
-void CreateCheckbox(HWND hWnd, HINSTANCE hInstance, int id, int yOffset, LPCWSTR text) {
-    CreateWindowW(L"BUTTON", text,
-        WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_AUTOCHECKBOX,
-        (250 - CHECKBOX_WIDTH) / 2, yOffset,
-        CHECKBOX_WIDTH, CHECKBOX_HEIGHT,
-        hWnd, reinterpret_cast<HMENU>(static_cast<INT_PTR>(id)), hInstance, nullptr);
-}
-
-static int getYPosition() {
-    static int initialOffset = 155;
-    return initialOffset += 30;
 }
 
 BOOL InitInstance(HINSTANCE hInstance, int nCmdShow) {
@@ -283,7 +121,6 @@ BOOL InitInstance(HINSTANCE hInstance, int nCmdShow) {
         hWnd, reinterpret_cast<HMENU>(7), hInstance, nullptr);
 
     CreateCheckbox(hWnd, hInstance, 6, getYPosition(), L"Boost game priority");
-    //CreateCheckbox(hWnd, hInstance, 7, getYPosition(), L"Spoof Version");
 
     ShowWindow(hWnd, nCmdShow);
     UpdateWindow(hWnd);
@@ -296,15 +133,13 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) 
 
     switch (message) {
     case WM_CREATE:
-    {
         hFont = CreateFont(-16, 0, 0, 0, FW_BOLD, TRUE, FALSE, FALSE,
             DEFAULT_CHARSET, OUT_DEFAULT_PRECIS,
             CLIP_DEFAULT_PRECIS, DEFAULT_QUALITY,
             DEFAULT_PITCH | FF_DONTCARE, L"");
         break;
-    }
-    case WM_PAINT:
-    {
+
+    case WM_PAINT: {
         PAINTSTRUCT ps;
         HDC hdc = BeginPaint(hWnd, &ps);
 
@@ -322,23 +157,22 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) 
         EndPaint(hWnd, &ps);
         break;
     }
-    case WM_COMMAND:
-    {
+
+    case WM_COMMAND: {
         int wmId = LOWORD(wParam);
         switch (wmId) {
-		case 1: // "Browse for Executable" button
+        case 1:
             BrowseForExecutable(hWnd);
             break;
-		case 2: // "Launch ITGmania" button
+        case 2:
             LaunchITGmania();
             break;
-		case 6: // "Boost game priority" checkbox
-        {
+        case 6: {
             LRESULT state = SendMessage(GetDlgItem(hWnd, 6), BM_GETCHECK, 0, 0);
             selectedPriority = (state == BST_CHECKED) ? REALTIME_PRIORITY_CLASS : NORMAL_PRIORITY_CLASS;
             break;
         }
-        case 7: // "Patch SL ver. check" button
+        case 7:
             if (wcslen(selectedExePath) > 0) {
                 SpoofExecutableVersion(selectedExePath);
             }
@@ -346,20 +180,165 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) 
                 MessageBox(hWnd, L"Please select an executable first.", L"Error", MB_OK | MB_ICONERROR);
             }
             break;
-        default: 
-			MessageBox(hWnd, L"Unknown command.", L"Error", MB_OK | MB_ICONERROR);
+        default:
+            MessageBox(hWnd, L"Unknown command.", L"Error", MB_OK | MB_ICONERROR);
             return DefWindowProc(hWnd, message, wParam, lParam);
         }
         break;
     }
+
     case WM_DESTROY:
         if (hFont) {
             DeleteObject(hFont);
         }
         PostQuitMessage(0);
         break;
+
     default:
         return DefWindowProc(hWnd, message, wParam, lParam);
     }
     return 0;
+}
+
+/*****************************************************************/
+/******************** Executable Modifiers ***********************/
+/*****************************************************************/
+void BrowseForExecutable(HWND hWnd) {
+    OPENFILENAME ofn;
+    WCHAR szFile[MAX_PATH];
+
+    ZeroMemory(&ofn, sizeof(ofn));
+    ofn.lStructSize = sizeof(ofn);
+    ofn.hwndOwner = hWnd;
+    ofn.lpstrFile = szFile;
+    ofn.lpstrFile[0] = '\0';
+    ofn.nMaxFile = sizeof(szFile) / sizeof(szFile[0]);
+    ofn.lpstrFilter = L"Executable Files\0*.exe\0All Files\0*.*\0";
+    ofn.nFilterIndex = 1;
+    ofn.Flags = OFN_PATHMUSTEXIST | OFN_FILEMUSTEXIST;
+
+    if (GetOpenFileName(&ofn) == TRUE) {
+        wcscpy_s(selectedExePath, ofn.lpstrFile);
+
+        HKEY hKey;
+        if (RegCreateKeyEx(HKEY_CURRENT_USER, REGISTRY_KEY, 0, nullptr, 0, KEY_WRITE, nullptr, &hKey, nullptr) == ERROR_SUCCESS) {
+            size_t dataSize = (wcslen(selectedExePath) + 1) * sizeof(WCHAR);
+            if (dataSize > MAXDWORD) {
+                MessageBox(nullptr, L"Path size exceeds the maximum allowed size. Please report this to the ITGmania Launcher GitHub.", L"Error", MB_OK | MB_ICONERROR);
+            }
+            else {
+                RegSetValueEx(hKey, REGISTRY_VALUE_NAME, 0, REG_SZ, reinterpret_cast<const BYTE*>(selectedExePath), static_cast<DWORD>(dataSize));
+            }
+            RegCloseKey(hKey);
+        }
+    }
+}
+
+void LaunchITGmania() {
+    HKEY hKey;
+    if (RegOpenKeyEx(HKEY_CURRENT_USER, REGISTRY_KEY, 0, KEY_READ, &hKey) == ERROR_SUCCESS) {
+        DWORD bufferSize = sizeof(selectedExePath);
+        if (RegQueryValueEx(hKey, REGISTRY_VALUE_NAME, nullptr, nullptr, (LPBYTE)selectedExePath, &bufferSize) != ERROR_SUCCESS) {
+            selectedExePath[0] = L'\0';
+        }
+        RegCloseKey(hKey);
+    }
+
+    if (wcslen(selectedExePath) == 0) {
+        MessageBox(nullptr, L"Please select the executable first.", L"Error", MB_OK | MB_ICONERROR);
+        return;
+    }
+
+    timeBeginPeriod(1);
+
+    SHELLEXECUTEINFO sei = { sizeof(SHELLEXECUTEINFO) };
+    sei.lpFile = selectedExePath;
+    sei.lpParameters = L"";
+    sei.nShow = SW_SHOWNORMAL;
+    sei.fMask = SEE_MASK_NOCLOSEPROCESS;
+
+    if (!ShellExecuteEx(&sei)) {
+        std::wcerr << L"Failed to launch application. Error: " << GetLastError() << std::endl;
+        return;
+    }
+
+    SetPriorityClass(sei.hProcess, selectedPriority);
+    WaitForSingleObject(sei.hProcess, INFINITE);
+    CloseHandle(sei.hProcess);
+    timeEndPeriod(1);
+}
+
+void SpoofExecutableVersion(const std::wstring& exePath) {
+    MessageBox(nullptr, L"This will patch out the version check in any Simply Love theme.\nThe next step will ask you to locate your Simply Love folder.\nIf you use Zmod, then use your Zmod folder.", L"Info", MB_OK | MB_ICONINFORMATION);
+
+    BROWSEINFO bi = { 0 };
+    bi.lpszTitle = L"Select the Simply Love (or Zmod) folder";
+    LPITEMIDLIST pidl = SHBrowseForFolder(&bi);
+
+    if (!pidl) {
+        MessageBox(nullptr, L"Scripts folder selection canceled.", L"Info", MB_OK | MB_ICONINFORMATION);
+        return;
+    }
+
+    WCHAR scriptsFolderPath[MAX_PATH];
+    if (!SHGetPathFromIDList(pidl, scriptsFolderPath)) {
+        MessageBox(nullptr, L"Failed to retrieve the Scripts folder path.", L"Error", MB_OK | MB_ICONERROR);
+        return;
+    }
+
+    CoTaskMemFree(pidl);
+
+    std::wstring luaFilePath = std::wstring(scriptsFolderPath) + L"\\Scripts\\SL-SupportHelpers.lua";
+
+    std::wifstream inputFile(luaFilePath);
+    if (!inputFile.is_open()) {
+        MessageBox(nullptr, L"Failed to open SL-SupportHelpers.lua for reading.", L"Error", MB_OK | MB_ICONERROR);
+        return;
+    }
+
+    std::wstring fileContent;
+    std::wstring line;
+    bool lineModified = false;
+
+    while (std::getline(inputFile, line)) {
+        if (line.find(L"local MinimumVersion") != std::wstring::npos) {
+            line = L"local MinimumVersion = {0, 0, 1}";
+            lineModified = true;
+        }
+        fileContent += line + L"\n";
+    }
+
+    inputFile.close();
+
+    if (!lineModified) {
+        MessageBox(nullptr, L"Target line not found in SL-SupportHelpers.lua.", L"Error", MB_OK | MB_ICONERROR);
+        return;
+    }
+
+    std::wofstream outputFile(luaFilePath, std::ios::trunc);
+    if (!outputFile.is_open()) {
+        MessageBox(nullptr, L"Failed to open SL-SupportHelpers.lua for writing.", L"Error", MB_OK | MB_ICONERROR);
+        return;
+    }
+
+    outputFile << fileContent;
+    outputFile.close();
+
+    MessageBox(nullptr, L"SL-SupportHelpers.lua modified successfully.", L"Success", MB_OK | MB_ICONINFORMATION);
+}
+
+/*****************************************************************/
+/************************ Helper Functions ***********************/
+/*****************************************************************/
+void CreateCheckbox(HWND hWnd, HINSTANCE hInstance, int id, int yOffset, LPCWSTR text) {
+    CreateWindowW(L"BUTTON", text,
+        WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_AUTOCHECKBOX,
+        (250 - CHECKBOX_WIDTH) / 2, yOffset,
+        CHECKBOX_WIDTH, CHECKBOX_HEIGHT,
+        hWnd, reinterpret_cast<HMENU>(static_cast<INT_PTR>(id)), hInstance, nullptr);
+}
+
+int getYPosition() {
+    static int initialOffset = 155;
+    return initialOffset += 30;
 }
--- ITGML/ITGML/ITGML.h
+++ ITGML/ITGML/ITGML.h
@@ -1,3 +1,36 @@
+// ITGML.h : Header file
+
 #pragma once
 
 #include "resource.h"
+#include <windows.h>
+#include <string>
+
+// Constants
+#define REGISTRY_KEY L"SOFTWARE\\ITGML"
+#define REGISTRY_VALUE_NAME L"ExecutablePath"
+#define MAX_LOADSTRING 100
+
+// Global Variables
+extern HINSTANCE hInst;
+extern WCHAR szTitle[MAX_LOADSTRING];
+extern WCHAR szWindowClass[MAX_LOADSTRING];
+extern WCHAR selectedExePath[MAX_PATH];
+extern int selectedPriority;
+
+// Function Declarations
+int getYPosition();
+void CreateCheckbox(HWND hWnd, HINSTANCE hInstance, int id, int yPos, LPCWSTR text);
+void SpoofExecutableVersion(const std::wstring& exePath);
+
+// Entry Point
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR lpCmdLine, _In_ int nCmdShow);
+
+// GUI Functions
+ATOM MyRegisterClass(HINSTANCE hInstance);
+BOOL InitInstance(HINSTANCE hInstance, int nCmdShow);
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
+
+// Executable Modifiers
+void BrowseForExecutable(HWND hWnd);
+void LaunchITGmania();
