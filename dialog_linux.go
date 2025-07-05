175665ef3dfacfbfefb280aa82dede48b7e80d9a
sukibaby <163092272+sukibaby@users.noreply.github.com>
2025-07-05 04:32:38 -0700

    Add a comment indicating doubt to dialog_linux.go

--- dialog_linux.go
+++ dialog_linux.go
@@ -10,7 +10,7 @@ import (
 func showUpdateDialog(latestVersion string) {
 	text := fmt.Sprintf("A new version (%s) is available! Go to GitHub?", latestVersion)
 	url := fmt.Sprintf("https://github.com/%s/%s/releases/latest", owner, repo)
-	cmd := exec.Command("dialog", "--yesno", text, "10", "50")
+	cmd := exec.Command("dialog", "--yesno", text, "10", "50") // I doubt this is the best way to do this. Is it?
 	err := cmd.Run()
 	if err == nil && cmd.ProcessState.Success() {
 		exec.Command("xdg-open", url).Start()
