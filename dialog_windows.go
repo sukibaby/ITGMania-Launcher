d09a086facc4de9d1d44db7a2219aee5673f5727
sukibaby <163092272+sukibaby@users.noreply.github.com>
2025-07-04 22:23:37 -0700

    Update dialog_windows.go

--- dialog_windows.go
+++ dialog_windows.go
@@ -12,15 +12,23 @@ import (
 func showUpdateDialog(latestVersion string) {
 	text := fmt.Sprintf("A new version (%s) is available!\nGo to GitHub?", latestVersion)
 	caption := "Update Available"
-	flags := uintptr(0x00000001) // MB_OKCANCEL
 
-	user32 := syscall.NewLazyDLL("user32.dll")
-	msgBox := user32.NewProc("MessageBoxW")
-	ret, _, _ := msgBox.Call(0,
-		uintptr(unsafe.Pointer(syscall.StringToUTF16Ptr(text))),
-		uintptr(unsafe.Pointer(syscall.StringToUTF16Ptr(caption))),
-		flags)
-	if int(ret) == 1 { // OK pressed
+	ret := messageBox(text, caption, 0x00000001)
+
+	// ret will be 1 if the user clicked OK
+	if ret == 1 {
 		exec.Command("rundll32", "url.dll,FileProtocolHandler", "https://github.com/itgmania/itgmania/releases/latest").Start()
 	}
+}
+
+func messageBox(text, caption string, flags uintptr) int {
+	user32 := syscall.NewLazyDLL("user32.dll")
+	msgBox := user32.NewProc("MessageBoxW")
+	ret, _, _ := msgBox.Call(
+		0,
+		uintptr(unsafe.Pointer(syscall.StringToUTF16Ptr(text))),
+		uintptr(unsafe.Pointer(syscall.StringToUTF16Ptr(caption))),
+		flags,
+	)
+	return int(ret)
 }
