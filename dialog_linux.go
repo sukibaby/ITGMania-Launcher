568559d665661836c88064f024f75c2b7549f535
sukibaby <163092272+sukibaby@users.noreply.github.com>
2025-07-04 22:23:40 -0700

    Update dialog_linux.go

--- dialog_linux.go
+++ dialog_linux.go
@@ -9,7 +9,7 @@ import (
 
 func showUpdateDialog(latestVersion string) {
 	text := fmt.Sprintf("A new version (%s) is available! Go to GitHub?", latestVersion)
-	cmd := exec.Command("zenity", "--question", "--title=Update Available", "--text="+text, "--ok-label=Go to GitHub", "--cancel-label=Maybe Later")
+	cmd := exec.Command("zenity", "--question", "--title=Update Available", "--text="+text)
 	err := cmd.Run()
 	if err == nil && cmd.ProcessState.Success() {
 		exec.Command("xdg-open", "https://github.com/itgmania/itgmania/releases/latest").Start()
