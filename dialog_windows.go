03624f8ab82ba1b6351425156b20e8fbfd0b37f7
sukibaby <163092272+sukibaby@users.noreply.github.com>
2025-07-05 03:01:49 -0700

    Add explanatory comments to Windows dialog functions
    Added comments to clarify the use of message box flags and the handling of return values in showUpdateDialog and messageBox functions

--- dialog_windows.go
+++ dialog_windows.go
@@ -14,6 +14,7 @@ func showUpdateDialog(latestVersion string) {
 	caption := "Update Available"
 	url := fmt.Sprintf("https://github.com/%s/%s/releases/latest", owner, repo)
 
+	// 0x00000001 is the MB_OKCANCEL flag, (message box with OK and Cancel buttons)
 	ret := messageBox(text, caption, 0x00000001)
 
 	// ret will be 1 if the user clicked OK
@@ -25,6 +26,11 @@ func showUpdateDialog(latestVersion string) {
 func messageBox(text, caption string, flags uintptr) int {
 	user32 := syscall.NewLazyDLL("user32.dll")
 	msgBox := user32.NewProc("MessageBoxW")
+
+	// StringToUTF16Ptr converts a Go string to a pointer to a UTF-16 encoded string
+	// so that it can be used with the Windows API.
+	// msgBox.call returns 3 values, but we only care about the first one (the return value).
+	// The second and third values are unused, so they are ignored by using underscores.
 	ret, _, _ := msgBox.Call(
 		0,
 		uintptr(unsafe.Pointer(syscall.StringToUTF16Ptr(text))),
