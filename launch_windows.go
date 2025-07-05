a1d64e511c80caadbe5c4af5f0ad3c8c0b91e0e4
sukibaby <163092272+sukibaby@users.noreply.github.com>
2025-07-05 05:37:17 -0700

    Add a comment for context

--- launch_windows.go
+++ launch_windows.go
@@ -11,6 +11,9 @@ const targetExe = "ITGmania.exe"
 
 func launchTarget() {
 	cmd := exec.Command(targetExe)
+	
+	// HideWindow is used to make sure no console window appears
 	cmd.SysProcAttr = &syscall.SysProcAttr{HideWindow: true}
+
 	cmd.Start()
 }
